be428ad865c0a93d93db12a45a8e634a9621a6b4
kien262004 <nkien6827@gmail.com>
2025-05-06 14:17:47 +0700

    update-11

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -510,22 +510,24 @@ class LatentDiffusion(DDPM):
         if not self.cond_stage_trainable:
             if config == "__is_first_stage__":
                 print("Using first stage also as cond stage.")
-                self.cond_stage_model = self.first_stage_model
+                cond_stage_model = self.first_stage_model
             elif config == "__is_unconditional__":
                 print(f"Training {self.__class__.__name__} as an unconditional model.")
-                self.cond_stage_model = None
+                cond_stage_model = None
                 # self.be_unconditional = True
             else:
                 model = instantiate_from_config(config)
-                self.cond_stage_model = model.eval()
-                self.cond_stage_model.train = disabled_train
-                for param in self.cond_stage_model.parameters():
+                cond_stage_model = model.eval()
+                cond_stage_model.train = disabled_train
+                for param in cond_stage_model.parameters():
                     param.requires_grad = False
+                
         else:
             assert config != '__is_first_stage__'
             assert config != '__is_unconditional__'
             model = instantiate_from_config(config)
-            self.cond_stage_model = model
+            cond_stage_model = model
+        return cond_stage_model
 
     def _get_denoise_row_from_list(self, samples, desc='', force_no_decoder_quantization=False):
         denoise_row = []
